7b7481f33de10abb54dc24201c1d4ca9f3e3a72d
angelgambin <56138071+angelgambin@users.noreply.github.com>
2020-07-26 19:24:04 +0200

    Add files via upload

---
+++ pizza/urls.py
@@ -0,0 +1,24 @@
+"""pizza URL Configuration
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include, path
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path("", include("orders.urls")), # get urls from 'orders' on this path
+    path("admin/", admin.site.urls), # get urls from admin of django
+    path('accounts/', include('django.contrib.auth.urls')) # get urls for login page
+]
